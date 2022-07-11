64bf5cec6e1621f34ef618f34e034d702a009054
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2022-07-11 23:34:53 +0700

    Correct cls version

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,3 +1,5 @@
 export { LoggerExceptionFilter } from 'src/logger/filters';
 export { GoogleCloudTransport } from 'src/logger/transports';
 export { createLogger } from 'src/logger/utilities';
+export { Logger } from 'src/logger/services'
+export { LoggerInterceptor } from 'src/logger/interceptors'
